91643b346f34512f072732c9cf672a55573063f4
Jeremy Dothager <jeremydothager@gmail.com>
2017-04-26 13:25:04 -0500

    Created Location class

---
+++ RandomRecord/Models/Location.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RandomRecord.Models
+{
+    public class Location
+    {
+        public string street { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public int zipcode { get; set; }
+    }
+}
