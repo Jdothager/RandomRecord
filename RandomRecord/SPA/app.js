f461d8d4710a7752bdf726387cd83d972bcc0746
Jeremy Dothager <jeremydothager@gmail.com>
2017-04-26 13:15:20 -0500

    added "New Record" button and functionality

--- RandomRecord/SPA/app.js
+++ RandomRecord/SPA/app.js
@@ -1,8 +1,14 @@
 ﻿var app = angular.module('App', []);
-app.controller('AppController', function ($scope, $http) {
-    $http.get("/api/record")
-    .then(function (response) {
-        $scope.record = response.data;
 
-    });
+app.controller('AppController', function ($scope, $http) {
+    $scope.record = null;
+
+    // get data from api
+    $scope.getNewRecord = function () {
+        $http.get("/api/record").then(function (response) {
+            $scope.record = response.data;
+        });
+    }
+
+    $scope.getNewRecord();
 })
